94d16b9cc6335f4701701f7b653fa6851274e6cb
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2016-09-21 14:03:14 +0300

    Update TM1637.cpp

--- TM1637/TM1637.cpp
+++ TM1637/TM1637.cpp
@@ -26,6 +26,8 @@
 //  Note: put sign for degree, letter 'r' & 'h'
 //  Date: 10 march, 2016
 //  Note: put sign for degree, letter 'r', 'h' & 'n'
+//  Date: 21 september, 2016
+//  Note: put letter 't', "u up and _ bottom" & "u bottom and - up" 
 /*******************************************************************************/
 #include "TM1637.h"
 #include <Arduino.h>
@@ -33,7 +35,9 @@ static int8_t TubeTab[] = {0x3f,0x06,0x5b,0x4f,
                            0x66,0x6d,0x7d,0x07,
                            0x7f,0x6f,0x77,0x7c,
                            0x39,0x5e,0x79,0x71,
-                           0x40,0x00,0x63,0x50,0x74,0x54 };//0~9,A,b,C,d,E,F,"-"," ",degree,r,h,n
+                           0x40,0x00,0x63,0x50,
+                           0x74,0x54,0x78,0x6a,0x1d };
+//0~9,A,b,C,d,E,F,"-"," ",degree,r,h,n,t, u with _, u with -
 
 TM1637::TM1637(uint8_t Clk, uint8_t Data)
 {
